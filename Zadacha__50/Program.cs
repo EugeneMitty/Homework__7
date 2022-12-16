9240e7dc43c74bdf1a2c68faeb3b53d177c3184c
Eugene <97278341+EugeneMtty@users.noreply.github.com>
2022-12-16 11:26:51 +0300

    Решение

---
+++ Zadacha__50/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число стодбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = new Random().Next(-10, 10);
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите строку элемента: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите столбец элемента: ");
+int column = Convert.ToInt32(Console.ReadLine());
+if (row < m && column < n) Console.WriteLine("Элемент массива с такими позициями существует: " + array[row, column]);
+else Console.WriteLine("Элемент массива с такими позициями не существует");
